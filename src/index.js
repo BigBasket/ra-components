2d6df74c06b0efaee54abcc09cfd472cda407118
sivaavkd <siva.adhikarla@bigbasket.com>
2021-07-14 15:46:50 +0530

    added inputemail

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ export { JsonField as JsonField } from './JSON/JsonField';
 export { JsonInput as JsonInput } from './JSON/JsonInput';
 
 export { TrimField as TrimField } from './Trim/TrimField';
+
+export {default as EmailInput} from './Email/InputEmail';
+
+export {EmailTypes as EmailTypes} from './Email/InputEmail';
