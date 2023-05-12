3cca8765f82a9cba8e771cc490fbb32aaf5ebb4c
ravi-ranjan-bb <raviranjan.kumar@bigbasket.com>
2023-05-12 14:16:03 +0530

    Resolved json parser issue

--- src/JSON/JsonInput.js
+++ src/JSON/JsonInput.js
@@ -3,16 +3,15 @@ import { TextInput } from 'react-admin';
 import isJSON from 'validator/lib/isJSON';
 
 const DEFAULT_ERRORTEXT = 'Invalid JSON';
-const nullreplacer =  (key,val) => (val === null? undefined:val);
-const parseFunction = (json) => {
+
+function parseFunction(json) {
   try {
     let retval = json;
-    if (retval && typeof retval === 'object') 
-      retval = JSON.stringify(retval,nullreplacer);
-    retval = JSON.parse(JSON.parse(retval));
+    if (retval && typeof retval === 'object')
+      retval = JSON.parse(retval);
     return retval;
   }
-  catch (e){return json;}
+  catch (e) { return json; }
 }
 
 /**
@@ -34,25 +33,37 @@ const parseFunction = (json) => {
   * <JsonInput source='config' label='JSON Config' parse={false}/>
  */
 export const JsonInput = (props) => {
-  const {validate = [],errortext = DEFAULT_ERRORTEXT,fullWidth=true, resettable = false,multiline = true,parseJSON=true} = props;
-  const errorobj = {message: errortext};
+  const { validate = [], 
+    errortext = DEFAULT_ERRORTEXT, 
+    fullWidth = true, 
+    resettable = false, 
+    multiline = true, 
+    parse = true,
+     ...rest } = props;
+  const errorobj = { message: errortext };
   const validateJSON = (value) => {
-    if (!value || typeof value === 'object') 
+    if (!value || typeof value === 'object')
       return undefined;
     return isJSON(value) ? undefined : errorobj;
   }
-  
+
   const formatJSON = (json) => {
     let retval = json;
-    if (retval && typeof retval === 'object') 
+    if (retval && typeof retval === 'object')
       retval = JSON.stringify(retval);
     return retval;
   };
-  const cProps = {fullWidth:fullWidth, resettable: resettable, multiline:multiline, validate:validate, format:formatJSON, parse:(parseJSON?parseFunction:undefined)};
-  Object.assign(cProps,props);
+  const cProps = {
+    fullWidth: fullWidth,
+    resettable: resettable,
+    multiline: multiline,
+    validate: validate,
+    format: formatJSON,
+    parse: parse ? parseFunction : undefined,
+  };
+
   validate.push(validateJSON);
   return (
-      <TextInput {...cProps}/>
+    <TextInput {...cProps} {...rest} />
   );
 }
-
